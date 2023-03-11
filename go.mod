4bfedb74a8969d59f773e8a1bfb65a7f7d917d4b
ChyiYaqing <chyiyaqing@gmail.com>
2023-03-11 09:32:30 +0800

    获取锁、释放锁、计数加一逻辑封装成方法

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/chyiyaqing/gotutorial.git
 
 go 1.19
-
-require github.com/chyiyaqing/gotutorial v0.0.0-20230311005609-160650ef84e1 // indirect
--- pkg/mutex/mutex.go
+++ pkg/mutex/mutex.go
@@ -5,20 +5,38 @@ import (
 	"sync"
 )
 
+type Counter struct {
+	// 嵌入字段
+	sync.Mutex
+	count uint64
+}
+
+// 计数器加1
+func (c *Counter) Incr() {
+	c.Lock()
+	c.count++
+	c.Unlock()
+}
+
+// 读取计数器值
+func (c *Counter) Count() uint64 {
+	c.Lock()
+	defer c.Unlock()
+	return c.count
+}
+
 func AdditionCount() {
-	var count = 0
-	// 使用WaitGroup等待10个goroutine完成
+	var counter Counter
 	var wg sync.WaitGroup
+	wg.Add(10)
 	for i := 0; i < 10; i++ {
 		go func() {
-			wg.Add(1)
 			defer wg.Done()
 			for j := 0; j < 100000; j++ {
-				// count++ 不是原子操作
-				count++
+				counter.Incr()
 			}
 		}()
 	}
 	wg.Wait()
-	fmt.Println(count)
+	fmt.Println(counter.Count())
 }
